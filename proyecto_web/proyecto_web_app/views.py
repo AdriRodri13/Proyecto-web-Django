e10d73414f40585da37afe3b8bae1ef34ff5bd6e
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2025-01-19 21:56:14 +0100

    Set Up Vistas

--- proyecto_web/proyecto_web_app/views.py
+++ proyecto_web/proyecto_web_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+#Vista de inicio
+def home(request):
+    return HttpResponse("Inicio")
+
+#Vista de servicios
+def services(request):
+    return HttpResponse("Servicios")
+
+#Vista de tienda
+def shop(request):
+    return HttpResponse("Tienda")
+
+#Vista de blog
+def blog(request):
+    return HttpResponse("Blog")
+
+#Vista de contacto
+def contact(request):
+    return HttpResponse("Contacto")
