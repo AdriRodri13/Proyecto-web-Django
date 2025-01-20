ac89b9b5695656cc241c136c3f2ad1d370938e15
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2025-01-20 16:57:22 +0100

    Primera App

---
+++ proyectoweb/proyectoWebApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from proyectoWebApp import views
+
+urlpatterns = [
+
+    path('', views.home, name='Inicio'),
+    path('servicios/', views.services, name='Servicios'),
+    path('tienda/', views.shop, name='Tienda'),
+    path('blog/', views.blog, name='Blog'),
+    path('contacto/', views.contact, name='Contacto'),
+
+]
--- proyectoweb/proyectoWebApp/views.py
+++ proyectoweb/proyectoWebApp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Inicio")
+
+def services(request):
+    return HttpResponse("Servicios")
+
+def shop(request):
+    return HttpResponse("Tienda")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def contact(request):
+    return HttpResponse("Contacto")
+
+
